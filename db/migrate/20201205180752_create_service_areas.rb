509ab8c2f17d58730cc80fd6b8ca340b860d2427
Mathew Trivett <mathew@wearesnook.com>
2020-12-05 18:09:28 +0000

    Adds database model for ServiceArea

---
+++ db/migrate/20201205180752_create_service_areas.rb
@@ -0,0 +1,10 @@
+class CreateServiceAreas < ActiveRecord::Migration[6.0]
+  def change
+    create_table :service_areas do |t|
+      t.string :name
+
+      t.timestamps
+    end
+    add_index :service_areas, :name
+  end
+end
