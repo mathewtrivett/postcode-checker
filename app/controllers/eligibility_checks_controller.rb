30d1f5cb575353f9fa99c675a995b721bbbb25b2
Mathew Trivett <mathew@wearesnook.com>
2020-12-05 17:58:56 +0000

    Outlines the core functionality for the postcode checker form

--- app/controllers/eligibility_checks_controller.rb
+++ app/controllers/eligibility_checks_controller.rb
@@ -6,5 +6,20 @@ class EligibilityChecksController < ApplicationController
     @eligibility_check = EligibilityCheck.new
   end
 
-  def create; end
+  def create
+    @eligibility_check = EligibilityCheck.new eligibility_params
+    if @eligibility_check.valid?
+      redirect_to success_eligibility_checks_path
+    else
+      render :new
+    end
+  end
+
+  def success; end
+
+  private
+
+  def eligibility_params
+    params.fetch(:eligibility_check, {}).permit(:postcode)
+  end
 end
--- spec/requests/eligibility_check_request_spec.rb
+++ spec/requests/eligibility_check_request_spec.rb
@@ -3,12 +3,36 @@
 require 'rails_helper'
 
 RSpec.describe '/eligibility_checks', type: :request do
-  describe 'GET /eligibility_checks/new' do
-    subject { response }
+  subject { response }
 
+  let(:valid_params) { attributes_for(:eligibility_check) }
+  let(:invalid_params) { attributes_for(:eligibility_check, postcode: nil) }
+
+  describe 'GET /eligibility_checks/new' do
     before { get new_eligibility_check_path }
 
     it { is_expected.to be_successful }
     it { is_expected.to render_template 'eligibility_checks/new' }
+  end
+
+  describe 'GET eligibility_checks/show' do
+    before { get success_eligibility_checks_path }
+
+    it { is_expected.to be_successful }
+    it { is_expected.to render_template 'eligibility_checks/success' }
+  end
+
+  describe 'POST /eligibility_checks' do
+    context 'with valid parameters' do
+      before { post eligibility_checks_path, params: { eligibility_check: valid_params } }
+
+      it { is_expected.to redirect_to success_eligibility_checks_path }
+    end
+
+    context 'with invalid parameters' do
+      before { post eligibility_checks_path, params: { eligibility_check: invalid_params } }
+
+      it { is_expected.to render_template 'eligibility_checks/new' }
+    end
   end
 end
