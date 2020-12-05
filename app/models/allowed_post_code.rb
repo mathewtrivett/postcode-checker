a40b77aa22336c6399a1513eaea5c24da1b68c53
Mathew Trivett <mathew@wearesnook.com>
2020-12-05 18:50:46 +0000

    Adds a script to seed the database with idempotency
    Also ran Rubocop to lint and format code.

--- app/models/allowed_post_code.rb
+++ app/models/allowed_post_code.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AllowedPostCode < ApplicationRecord
   validates :postcode, presence: true
   belongs_to :service_area, optional: true
--- app/models/service_area.rb
+++ app/models/service_area.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
 class ServiceArea < ApplicationRecord
   validates :name, presence: true
   has_many :allowed_post_codes, dependent: :destroy
+  accepts_nested_attributes_for :allowed_post_codes
 end
