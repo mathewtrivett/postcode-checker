70bcd4802af5e05a5069a3a8e27465bd71218063
Mathew Trivett <mathew@wearesnook.com>
2020-12-06 21:41:52 +0000

    Refactored attributes out into method calls

--- app/validators/allowed_post_code_validator.rb
+++ app/validators/allowed_post_code_validator.rb
@@ -4,24 +4,35 @@ require 'net/http'
 
 # Custom Validator to validate allowed postcodes
 class AllowedPostCodeValidator < ActiveModel::EachValidator
+  attr_reader :response, :value
+
   # Validates the arguments to check if they are an allowed postcode
   # @param record [ActiveModel|ActiveRecord]
   # @param attribute [Symbol]
   # @param value [Object]
   def validate_each(record, attribute, value)
-    response = AllowedPostCodeValidator.fetch_postcode(value) if value.present?
-    record.errors.add(attribute, :not_in_service) unless value.present? && valid_service_area?(response, value)
+    @value = value
+    @response = AllowedPostCodeValidator.fetch_postcode(value) if @value.present?
+    record.errors.add(attribute, :not_in_service) unless value.present? && valid_service_area?(@response, @value)
   end
 
   # Validates if the postcode can be served
   # @param value [Object] The value passed to the validator
   # @param response [Net::HTTPResponse]
   # @return [Boolean]
-  def valid_service_area?(response, value)
-    lsoa_code = response.dig(:result, :lsoa) if response.present?
-    lsoa = lsoa_code.nil? ? nil : lsoa_code[..-6].downcase
-    postcode = value.upcase.upcase.gsub(/\s+/, '') if value
-    ServiceArea.exists?(['lower(name) = ?', lsoa]) || AllowedPostCode.exists?(postcode: postcode)
+  def valid_service_area?(_response, _value)
+    ServiceArea.exists?(['lower(name) = ?', lsoa_code]) || AllowedPostCode.exists?(postcode: postcode)
+  end
+
+  # Returns a formatted lsoa code
+  def lsoa_code
+    code = response.dig(:result, :lsoa) if response.present?
+    code.nil? ? nil : code[..-6].downcase
+  end
+
+  # Returns a formatted postcode
+  def postcode
+    value.upcase.upcase.gsub(/\s+/, '') if value
   end
 
   # Creates a simple client to query the postcode service
