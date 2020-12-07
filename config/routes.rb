cb9e489a1330232ed243d03dc9086061130ece07
Mathew Trivett <mathew@wearesnook.com>
2020-12-07 16:53:55 +0000

    Adds a dashboard to manage allowed postcodes and service areas

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :service_areas
+    resources :allowed_post_codes
+
+    root to: 'service_areas#index'
+  end
   root 'eligibility_checks#new'
   resources :eligibility_checks, only: %i[new create] do
     get 'success', on: :collection
