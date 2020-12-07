80a65d4a470c447d6b4aacfb3c39d738cbb5a72e
Mathew Trivett <mathew@wearesnook.com>
2020-12-07 13:33:09 +0000

    Cleans up the postcode validator code

--- app/validators/allowed_post_code_validator.rb
+++ app/validators/allowed_post_code_validator.rb
@@ -13,26 +13,26 @@ class AllowedPostCodeValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     @value = value
     @response = AllowedPostCodeValidator.fetch_postcode(value) if @value.present?
-    record.errors.add(attribute, :not_in_service) unless value.present? && valid_service_area?(@response, @value)
+    record.errors.add(attribute, :not_in_service) unless value.present? && valid_service_area?
   end
 
   # Validates if the postcode can be served
   # @param value [Object] The value passed to the validator
   # @param response [Net::HTTPResponse]
   # @return [Boolean]
-  def valid_service_area?(_response, _value)
+  def valid_service_area?
     ServiceArea.exists?(['lower(name) = ?', lsoa_code]) || AllowedPostCode.exists?(postcode: postcode)
   end
 
   # Returns a formatted lsoa code
   def lsoa_code
-    code = response.dig(:result, :lsoa) if response.present?
-    code.nil? ? nil : code[..-6].downcase
+    lsoa = response.dig(:result, :lsoa) if response.present?
+    lsoa.nil? ? nil : lsoa[..-6].downcase
   end
 
   # Returns a formatted postcode
   def postcode
-    value.upcase.upcase.gsub(/\s+/, '') if value
+    value&.upcase&.gsub(/\s+/, '')
   end
 
   # Creates a simple client to query the postcode service
